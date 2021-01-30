faca955e0dfe519e5113c35a40e2416014fe28cc
Teodoro Garcia <tedtke514@gmail.com>
2021-01-30 16:30:19 -0600

    commit tree 5ccc3c7f50dc5d366bcc6c460be3ef4270fb193b

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
 
 // Write your code here
+
+let num1 = 1;
+let num2 = 62;
+let multiply = num1 * num2;
+
+let random = Math.ceil(Math.random() * 10) + 1;
+random;
+
+let mod = 16 % 6;
+mod;
+
+let max = Math.max(1, 2, 4, 20);
+max;
